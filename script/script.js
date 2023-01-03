d584d557b36c8543f24dbf01ce3b8c634142efe7
adrian411997 <adrian.mart41197@gmail.com>
2023-01-02 21:58:04 -0300

    last changes

--- script/script.js
+++ script/script.js
@@ -3,3 +3,7 @@ const rotated = document.getElementById("cv");
 function rotate() {
   rotated.style.transform = "rotateY(180deg)";
 }
+function toOriginal() {
+  console.log("rotate");
+  rotated.style.transform = "rotateY(360deg)";
+}
